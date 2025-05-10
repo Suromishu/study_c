c08c846fc71f3590bd520eb98830459af2e1d19d
Mishu <152959369+Suromishu@users.noreply.github.com>
2025-05-10 09:49:27 +0800

    Initial commit

---
+++ src/demo1/demo06.c
@@ -0,0 +1,24 @@
+﻿//
+// Created by Mishu on 2025/5/10.
+//
+
+#include <stdio.h>
+
+int fibonacci(int n)
+{
+    if (n <= 1)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void print_fibonacci(int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d\n", fibonacci(i));
+}
+
+int main()
+{
+    print_fibonacci(10);
+}
