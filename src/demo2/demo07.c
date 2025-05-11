4561bfc55fe242884aa79613b8c4db9d908997ee
Mishu <152959369+Suromishu@users.noreply.github.com>
2025-05-11 23:25:39 +0800

    Initial commit

---
+++ src/demo2/demo07.c
@@ -0,0 +1,20 @@
+﻿//
+// Created by Mishu on 2025/5/11.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int i = *arr;
+    printf("%d\n", i);
+    printf("%d\n", arr[0]);
+
+    printf("%d\n", *(arr + 1));
+
+    int *a = arr + 3;
+    printf("%d\n", *a);
+    return 0;
+}
